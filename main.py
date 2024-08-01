6450c6620ec954c2903eb7bf558eb674b31aee60
Jay Ravalani <jay.ravalani746@gmail.com>
2024-08-01 19:51:48 +0530

    add different url end points

--- main.py
+++ main.py
@@ -3,13 +3,29 @@ import pandas as pd
 
 app = Flask("__name__")
 
+stations = pd.read_csv("data_small/stations.txt", header=0, skiprows=17)
+stations = stations[["STAID", "STANAME                                 "]][0:100]
+stations = stations.to_html()
+
 # important to store all .html files inside "templates" folder and images inside "static" folder
 # @ indicates that the following line is a declarator, and it declares the .route() method to the home() function.
 
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("home.html", data=stations, )
+
+
+@app.route("/api/v1/<station>/<year>")
+def year_data(station, year):
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    station_df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    station_df['YEAR'] = station_df['    DATE'].dt.year
+    station_df = station_df.loc[station_df['YEAR'] == int(year)]
+    result = station_df.to_dict(orient="records")
+    # OR
+    # result = station_df.loc[station_df['    DATE'].dt.year == int(year)].to_dict(orient="records")
+    return result
 
 
 @app.route("/api/v1/<station>/<date>")
@@ -21,6 +37,14 @@ def data(station, date):
     return {'station': station,
             'date': date,
             'temperature': temperature}
+
+
+@app.route("/api/v1/<station>")
+def station_data(station):
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    station_df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    result = station_df.to_dict(orient="records")
+    return result
 
 
 if __name__ == "__main__":
